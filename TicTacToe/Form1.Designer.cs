d5c80bdefc09c4f83c9446e9061c24b728fb69b4
Aloysius Butac <39267018+AloysiusButac@users.noreply.github.com>
2020-08-05 00:36:43 +0800

    Add files via upload

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start_button = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x_count = new System.Windows.Forms.Label();
+            this.o_count = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label_turn = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(4, 3);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(131, 46);
+            this.start_button.TabIndex = 0;
+            this.start_button.Text = "START";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(571, 30);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 26);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.button_reset);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // x_count
+            // 
+            this.x_count.AutoSize = true;
+            this.x_count.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x_count.Location = new System.Drawing.Point(33, 48);
+            this.x_count.Name = "x_count";
+            this.x_count.Size = new System.Drawing.Size(71, 48);
+            this.x_count.TabIndex = 11;
+            this.x_count.Text = "X :";
+            // 
+            // o_count
+            // 
+            this.o_count.AutoSize = true;
+            this.o_count.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.o_count.Location = new System.Drawing.Point(182, 48);
+            this.o_count.Name = "o_count";
+            this.o_count.Size = new System.Drawing.Size(65, 48);
+            this.o_count.TabIndex = 12;
+            this.o_count.Text = "O:";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(19, 119);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(399, 370);
+            this.flowLayoutPanel1.TabIndex = 13;
+            this.flowLayoutPanel1.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.start_button);
+            this.panel1.Location = new System.Drawing.Point(424, 186);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(135, 303);
+            this.panel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label_turn
+            // 
+            this.label_turn.AutoSize = true;
+            this.label_turn.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_turn.Location = new System.Drawing.Point(356, 50);
+            this.label_turn.Name = "label_turn";
+            this.label_turn.Size = new System.Drawing.Size(0, 46);
+            this.label_turn.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 513);
+            this.Controls.Add(this.label_turn);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.o_count);
+            this.Controls.Add(this.x_count);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label x_count;
+        private System.Windows.Forms.Label o_count;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label_turn;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool is_player_1 = true;
+        Button[] tto = new Button[9];
+        int x_win = 0;
+        int o_win = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("working");
+            //
+            // Create new Buttons and set name, size
+            //
+            for(int i = 0; i < tto.Length; i++)
+            {
+                tto[i] = new Button();
+                tto[i].Size = new Size(90, 90);
+
+            }
+            for (int i = 0; i < tto.Length; i++)
+            {
+                tto[i].Click += new EventHandler(button_click);
+                string name = $"square{i}";
+                tto[i].Name = name;
+                tto[i].Font = new Font("Microsoft Sans Serif", 50, FontStyle.Bold);
+
+                flowLayoutPanel1.Controls.Add(tto[i]);
+                Console.WriteLine("{0} added", tto[i].Name);
+            }
+            flowLayoutPanel1.Visible = true;
+
+            // hides button
+            Button start_button = (Button)sender;
+            start_button.Enabled = false;
+            start_button.Visible = false;
+            // resets button
+            Button reset_button = new Button();
+            reset_button.Size = new Size(100, 40);
+            reset_button.Text = "RESET";
+            reset_button.Click += new EventHandler(button_reset);
+            panel1.Controls.Add(reset_button);
+
+            label_turn.Text = "X's Turn";
+                       
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+
+            Button b = (Button)sender;
+
+            if (b.Text == "") {
+                if (is_player_1)
+                {
+                    b.Text = "X";
+                    label_turn.Text = "O's Turn";
+                } else
+                {
+                    b.Text = "O";
+                    label_turn.Text = "X's Turn";
+                }
+
+                check_winner();
+                check_draw();
+                turn_swap();
+            } else {
+                MessageBox.Show("Invalid square", "Choose an empty square");
+            }
+        }
+
+        private void button_reset(object sender, EventArgs e)
+        {
+            for(int i = 0; i < tto.Length; i++)
+            {
+                tto[i].Enabled = true;
+                tto[i].Text = "";
+            }
+            label_turn.Text = "X's Turn";
+        }
+
+        private void check_winner()
+        {
+            // horizontal
+            bool top = (tto[0].Text == tto[1].Text) && (tto[1].Text == tto[2].Text) && tto[1].Text != "";
+            bool midh = (tto[3].Text == tto[4].Text) && (tto[4].Text == tto[5].Text) && tto[4].Text != "";
+            bool bot = (tto[6].Text == tto[7].Text) && (tto[7].Text == tto[8].Text) && tto[7].Text != "";
+            bool horizontal = top || midh || bot;
+            // vertical
+            bool left = (tto[0].Text == tto[3].Text) && (tto[3].Text == tto[6].Text) && tto[3].Text != "";
+            bool midv = (tto[1].Text == tto[4].Text) && (tto[4].Text == tto[7].Text) && tto[4].Text != "";
+            bool right = (tto[2].Text == tto[5].Text) && (tto[5].Text == tto[8].Text) && tto[5].Text != "";
+            bool vertical = left || midv || right;
+            // diagonals
+            bool leftd = (tto[0].Text == tto[4].Text) && (tto[4].Text == tto[8].Text) && tto[4].Text != "";
+            bool rightd = (tto[2].Text == tto[4].Text) && (tto[4].Text == tto[6].Text) && tto[4].Text != "";
+            bool diagonal = leftd || rightd;
+
+            string winner = "";
+            if (horizontal || vertical || diagonal)
+            {
+
+                if(horizontal)
+                {
+                    if(top)
+                    {
+                        winner = tto[1].Text;
+                    } else if(midh)
+                    {
+                        winner = tto[4].Text;
+                    } else
+                    {
+                        winner = tto[7].Text;
+                    }
+                } 
+                else if(vertical)
+                {
+                    if (left)
+                    {
+                        winner = tto[3].Text;
+                    }
+                    else if (midv)
+                    {
+                        winner = tto[4].Text;
+                    }
+                    else
+                    {
+                        winner = tto[5].Text;
+                    }
+                } 
+                else
+                {
+                    winner = tto[4].Text;
+                }
+
+                if(winner == "X")
+                {
+                    x_win += 1;
+                    x_count.Text = $"X: {x_win}";
+                } else if (winner == "O")
+                {
+                    o_win += 1;
+                    o_count.Text = $"O: {o_win}";
+                }
+
+                foreach (Button b in tto)
+                {
+                    b.Enabled = false;
+                }
+                MessageBox.Show($"Congrats {winner}, You Won!", "Winner!");
+                is_player_1 = true;
+            }
+        }
+
+        private void check_draw()
+        {
+            int count = 0;
+            bool draw = false;
+            for (int i = 0; i < 9; i++)
+            {
+                if (tto[i].Text == "")
+                {
+                    draw = false;
+                    continue;
+                } else {
+                    count++;
+                }
+            }
+
+            if(draw)
+            {
+                foreach (Button b in tto)
+                {
+                    b.Enabled = false;
+                }
+                MessageBox.Show($"Oh no! A Draw", "Draw");
+            }
+        }
+
+        private void turn_swap()
+        {
+            if(is_player_1)
+            {
+                is_player_1 = false;
+            } else
+            {
+                is_player_1 = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            Button d = (Button)sender;
+            System.Windows.Forms.Button[] b = new System.Windows.Forms.Button[2];
+
+                b[0] = new System.Windows.Forms.Button();
+                b[1] = new System.Windows.Forms.Button();
+
+            int posy = 10;
+            int i = 1;
+            foreach(Button a in b)
+            {
+                a.Size = new Size(30, 30);
+                a.Location = new Point(10, posy);
+                flowLayoutPanel1.Controls.Add(a);
+
+                posy += 40;
+            }
+
+            d.Enabled = false;
+            d.Visible = false;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+        }
+    }
+}
